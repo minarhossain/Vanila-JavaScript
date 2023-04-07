c7b83b9ca0913858d93b38f73ef76c3fa18816fa
minarhossain <codewithmnr@gmail.com>
2023-04-07 14:17:57 +0600

    Out put system of JS

---
+++ Lesson2/OutPut.js
@@ -0,0 +1,18 @@
+// JavaScript display possibilities
+// JavaScript can display data in different ways
+// 1. Writing in to an HTML element , using innerHTML
+// 2. Writing into the HTML Output using document.write()
+// 3. Writing into an alert box, using window.alert()
+// 4. Writing into the browser consol, using console.log()
+
+
+// Using innerHTML
+// To access an HTML element JavaScript can use the document.getElementById(id) method
+// The id attribute defines the HTML element. The innerHTML property defines the HTML content
+
+
+const id = document.getElementById("id name in string")
+const cla = document.getElementsByClassName(" class name in string")
+
+// document.write() is used for testing
+// You can use an alert box to display data using window.alert()
