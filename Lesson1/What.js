8654c3116f1f9a0c06846768408ece4123cc441a
minarhossain <codewithmnr@gmail.com>
2023-04-07 12:57:17 +0600

    Vanila JS Start

---
+++ Lesson1/What.js
@@ -0,0 +1,23 @@
+// Programming Language
+// Programming Language for Web
+// JavaScript is easy to Learn
+// JavaScript is one of the 3 languages all web developers must learn (HTML, CSS, JS)
+// ES1, ES2,ES3 (1997-1999)
+// ES5(2009)
+// ES6(2015)
+// JavaScript can changed HTML content
+
+
+// JS can Change HTML attribute
+<img src="" alt="" id="" />  // src, alt, id all are attributes
+
+// JS can change HTML Style (CSS)
+
+document.getElementById('').style.fontsize = "36px";
+
+// JS can show HTML Element
+
+// JS Where to
+
+// in HTML file , <head> </head>  <body> </body>  We can write JS
+
